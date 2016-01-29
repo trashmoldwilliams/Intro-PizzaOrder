d1e7ccbcea1182b4e29928d5f8009d75879e3452
Will <student@epicodus.com>
2016-01-29 09:00:49 -0800

    Add constructors and specs for toppings, sizes, and pizzas.

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+function Topping(name, price) {
+  this.name = name;
+  this.price = price;
+}
+
+function Size(name, price) {
+  this.name = name;
+  this.price = price;
+}
+
+function Pizza(toppings, size) {
+  this.toppings = toppings;
+  this.size = size;
+}
---
+++ spec/specs.js
@@ -0,0 +1,25 @@
+describe('Topping', function() {
+  it("will construct a new topping with the given properties", function() {
+    var testTopping = new Topping("Pepperoni", 2);
+    expect(testTopping.name).to.equal("Pepperoni");
+    expect(testTopping.price).to.equal(2);
+  });
+});
+
+describe('Size', function() {
+  it("will construct a new size with the given properties", function() {
+    var testSize = new Size("Large", 10);
+    expect(testSize.name).to.equal("Large");
+    expect(testSize.price).to.equal(10);
+  });
+});
+
+describe('Pizza', function() {
+  it("will construct a new pizza with the given properties", function() {
+    var testTopping1 = new Topping("Pepperoni", 2);
+    var testTopping2 = new Topping("Olives", 1);
+    var testPizza = new Pizza([testTopping1, testTopping2], "large");
+    expect(testPizza.toppings).to.eql([testTopping1, testTopping2]);
+    expect(testPizza.size).to.equal("large");
+  });
+});
